669e74afd5355fa543b9d60de7e03c3060dc568b
Arthur Kwak <arthurmichielkwak@gmail.com>
2024-10-23 15:03:28 +0200

    Fix logup and add beginning of the password manager

--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Add the parent directory to the system path
-from main import startAuthenticate
 import re
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Add the parent directory to the system path
+
 
 app = Flask(__name__)
 
@@ -20,25 +20,29 @@ def handle_data():
     
     return jsonify({"error": "Invalid method"}), 400
 
-
 def authenticate():
+    from main import start
     account_owner = request.args.get('account_owner')
     username = request.args.get('username')
     password = request.args.get('password')
     code_totp = request.args.get('code')
     if username and password and code_totp:
-        result = startAuthenticate(account_owner, username, password, code_totp)
+        result = start("authenticate", account_owner, username, password, code_totp)
         pattern = r'Too many failed login attempts. Please try again in \d{2}:\d{2}\ '
-        if result == 'logged in':
+        if result == 'Logged in':
             return jsonify({"message": "Authentication successful"})
-        elif result == 'wrong username, authentication code or password':
+        elif result == 'Wrong credentials':
             return jsonify({"error": "Authentication failed"}), 401
         elif re.match(pattern, result):
             return jsonify({"error": result}), 401
         else:
             return jsonify({"error": "Internal server error"}), 500
-            
     return jsonify({"error": "missing arguments"}), 400
+def password():
+    from main import start
+    
+def start_server():
+    app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    start_server()
--- main.py
+++ main.py
@@ -1,30 +1,53 @@
-from core import logup
-
+from core import logup, verifyArgs, passwordmanage
+from api import start_server
 class Main:
-    def start(account_owner, username, password, code):
+    def startLogup(account_owner, username, password, code):
+        def login(username, password, code, masterpass=False):
+            loggedIn, masterPassword, log = logup.login(username, password, code)
+            if log == True:
+                if loggedIn:
+                    logup.log(username, 1, "login")
+                    if masterPassword:
+                        return (True, '600', masterPassword)
+                    else:
+                        return (True, '600')
+                else: 
+                    logup.log(username, 0, "login")
+                    if masterPassword:
+                        return (False, '700', masterPassword)
+                    else:
+                        return (False, '700')
+            else:
+                return log
+
+        def register(username, password, code):
+            logup.register(username, password)
+            return login(username, password, code)
+
         if account_owner:
-            return Main.login(username, password, code)
+            return login(username, password, code)
         else: 
-            return Main.register(username, password, code)
-
-    def login(username, password, code):
-        loggedIn, masterPassword, log = logup.login(username, password, code)
-        if log == True:
-            if loggedIn:
-                logup.log(username, 1, "login")
-                return '600'
-            else: 
-                logup.log(username, 0, "login")
-                return '700'
+            return register(username, password, code)
+    def startPassword(method, username, password, code, accountname='', accountpassword=''):
+        Verify, masterpassword, code = Main.startLogup.login(username, password, code, masterpass=True)
+        if method == "add":
+            if code == "600":
+                passwordmanage.addpass(username, masterpassword, accountname, accountpassword)
+        elif method == "get":
+            return logup.getpass(username, password, code)
+        elif method == "delete":
+            return logup.deletepass(username, password, code)
+        elif method == "update":
+            return logup.updatepass(username, password, code)
         else:
-            return log
+            return "Invalid method"
+def start(method, account_owner, username, password, code):
+    if method == "authenticate":
+        args = Main.startLogup(account_owner, username, password, int(code))
+        translated_args = verifyArgs('return', args)
+        return translated_args
+    elif method == "password":
+        args = Main.startPassword(method, username, password, code)
 
-    def register(account_owner, username, password, code):
-        logup.register(username, password)
-        return Main.login(username, password, code)
-
-def startAuthenticate(account_owner, username, password, code):
-    return Main.start(account_owner, username, password, int(code))
 if __name__ == '__main__':
-    x = startAuthenticate("True", "username", "password", "000000")
-    print(x)
+    start_server()
