92ea91912510b0c1df8c87ff5c60ac3aa5153669
Tureluurtje <tureluurtje.1@gmail.com>
2025-07-12 15:38:20 +0200

    Add sidebar

--- web/index.js
+++ web/index.js
@@ -1,1 +1,19 @@
 console.log("Token:", window.AUTH_TOKEN);
+
+function setView(category, clickedElement) {
+  const items = document.querySelectorAll('.content-item');
+  
+  items.forEach(item => {
+    if (category === 'all' || item.getAttribute('data-category') === category) {
+      item.classList.remove('hidden');
+    } else {
+      item.classList.add('hidden');
+    }
+  });
+document.querySelectorAll('.sidebar__section__item').forEach(item => {
+    item.classList.remove('active');
+  });
+
+  // Add 'active' to the clicked one
+  clickedElement.classList.add('active');
+}
